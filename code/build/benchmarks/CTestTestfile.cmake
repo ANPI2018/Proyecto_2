98216d488f116fb3881fbe87d9d0148d023ff927
PABUMO <pablex.bumo@gmail.com>
2018-10-07 20:15:25 -0600

    Creación de la clase ResistorGrid y de sus pruebas unitarias.

--- code/build/benchmarks/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pablo/Documents/ANPI/Proyecto2/Proyecto_2/code/benchmarks
-# Build directory: /home/pablo/Documents/ANPI/Proyecto2/Proyecto_2/code/build/benchmarks
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(benchmark "/home/pablo/Documents/ANPI/Proyecto2/Proyecto_2/code/build/bin/benchmark")
